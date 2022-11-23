46725462090af3fbc09030d73793af9014ce4046
parastoo soori <parastoosoori@parastoos-Air.gateway.sasktel.net>
2022-11-23 00:42:19 -0600

    firt try

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms.validators import DataRequired, AnyOf, URL,Regexp
 
 class ShowForm(Form):
+    
     artist_id = StringField(
         'artist_id'
     )
@@ -83,8 +84,10 @@ class VenueForm(Form):
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone'
+        'phone',validators=[DataRequired(), Regexp("^[0-9]*$", message="Phone number should only contain digits")]
     )
+        
+    
     image_link = StringField(
         'image_link'
     )
@@ -193,7 +196,7 @@ class ArtistForm(Form):
     )
     phone = StringField(
         # TODO implement validation logic for state
-        'phone'
+        'phone', validators=[DataRequired(), Regexp("^[0-9]*$", message="Phone number should only contain digits")]
     )
     image_link = StringField(
         'image_link'
